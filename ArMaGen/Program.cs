72e30edc0c609cc02920a3f5f64af44c243aaab0
therealtbs <therealtbs@gmail.com>
2014-04-16 18:53:48 +0200

    Initial Commit

---
+++ ArMaGen/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ArMaGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var files = Directory.EnumerateFiles(Environment.CurrentDirectory, args[0]);
+            File.WriteAllText(Path.Combine(Environment.CurrentDirectory, "artifacts.json"), JsonConvert.SerializeObject(new { artifacts = files }));
+        }
+    }
+}
